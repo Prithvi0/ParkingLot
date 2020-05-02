c619118ebd931757852ddf38387cccc0fe50b354
Prithvi0 <prithviy96@gmail.com>
2020-05-02 05:37:55 +0530

    Modified the function to push an object & added return statement

--- main/parkingLot.js
+++ main/parkingLot.js
@@ -1,7 +1,13 @@
 // class instantiation
 class ParkingLot {
-    // initialising empty function
-    parkTheCar() { }
+    constructor() {
+        this.ParkingLot = new Array();
+    }
+    // initialising function to park the vehicle
+    parkVehicle(car) {
+        this.ParkingLot.push(car);
+        return true;
+    }
 }
 
 module.exports = ParkingLot;
