aed8c8f25326201687d32c600dd4dff1face55a8
Prithvi0 <prithviy96@gmail.com>
2020-05-02 07:28:03 +0530

    Added function to unpark vehicle & passed a failed testcase

--- main/parkingLot.js
+++ main/parkingLot.js
@@ -3,11 +3,13 @@ class ParkingLot {
     constructor() {
         this.ParkingLot = new Array();
     }
-    // initialising function to park the vehicle
+    // function to park the vehicle
     parkVehicle(car) {
         this.ParkingLot.push(car);
         return true;
     }
+    // function to unpark the vehicle
+    unparkVehicle(car) { }
 }
 
 module.exports = ParkingLot;
--- test/parkingLotTest.js
+++ test/parkingLotTest.js
@@ -5,17 +5,28 @@ const parkingLotSystem = require('../main/parkingLot');
 const parkingLotArea = new parkingLotSystem();
 
 // Test cases for parking lot system
-// U.C 1: Make sure driver is able to park their car to catch flight
-describe('Parking lot system tests', () => {
-    // Test case for vehicle parking
-    it('should verify parking lot system', () => {
-        car = new Object();
-        assert.isUndefined(parkingLotArea.parkVehicle().vehicle, "Verify parking lot");
+describe('Parking lot system tests:', () => {
+    // U.C 1: Make sure driver is able to park their car to catch flight
+    describe('\n\tTests for Parking vehicles:', () => {
+        // T.C 1.1: Test case for vehicle parking
+        it('should verify parking lot system', () => {
+            car = new Object();
+            assert.isUndefined(parkingLotArea.parkVehicle().car, "Verify parking lot");
+        });
+
+        // T.C 1.2: Test case to return true when car is parked
+        it('should return true when car is parked', () => {
+            car = new Object();
+            assert.isTrue(parkingLotArea.parkVehicle(car));
+        });
     });
 
-    // Test case to return true when car is parked
-    it('should return true when car is parked', () => {
-        car = new Object();
-        assert.isTrue(parkingLotArea.parkVehicle(car));
-    })
+    // U.C 2: Make sure driver is able to unpark their car to return home
+    describe('\n\tTests for Unparking vehicles:', () => {
+        // T.C 2.1: Test case for vehicle unparking
+        it('should verify unparking vehicle', () => {
+            car = new Object();
+            assert.isUndefined(parkingLotArea.unparkVehicle(car));
+        });
+    });
 });
