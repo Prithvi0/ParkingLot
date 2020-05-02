f629b3ad826a574ddd028bca05c97e9585a8334c
Prithvi0 <prithviy96@gmail.com>
2020-05-02 07:57:04 +0530

    Added the same code again after merge issue

--- main/parkingLot.js
+++ main/parkingLot.js
@@ -8,6 +8,8 @@ class ParkingLot {
         this.ParkingLot.push(car);
         return true;
     }
+    // function to unpark the vehicle
+     unparkVehicle(car) { }
 }
 
 module.exports = ParkingLot;
