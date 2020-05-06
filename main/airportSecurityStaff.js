ed6a925534195dece900d31fb14d60397befbfd2
Prithvi0 <prithviy96@gmail.com>
2020-05-06 22:00:01 +0530

    Added program file to inform airport security staff when parkinglot full

---
+++ main/airportSecurityStaff.js
@@ -0,0 +1,12 @@
+const parkingLotSystem = require('../main/parkingLot');
+
+class AirportSecurityPersonal {
+    constructor() {
+        parkingLotSystem.length === 1;
+    }
+    parkingFull() {
+        return parkingLotSystem.length;
+    }
+}
+
+module.exports = new AirportSecurityPersonal();
